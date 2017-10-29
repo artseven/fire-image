4e38d141218bdeeffb2b8d37f508701c4f43396f
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-10-29 15:40:21 -0400

    img array added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  images = [
+      {
+        FILE: "http://www.solosnet.com/signage/vertical12up90.jpg",
+        DATETIME: 1508944716
+      },
+      {
+        FILE: "http://www.solosnet.com/signage/vertical1up90.jpg",
+        DATETIME: 1508944836
+      },
+      {
+        FILE: "http://www.solosnet.com/signage/america.gif",
+        DATETIME: 1508945016
+      }
+  ];
   constructor(public navCtrl: NavController) {
 
   }
