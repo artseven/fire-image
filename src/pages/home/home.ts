661144bfee5d4916d78acd1db12c330c8ae870c6
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-13 16:11:39 -0500

    adding timer function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,9 +12,9 @@ export class HomePage {
   @ViewChild(Slides) slides:Slides
   fetchedImages: any;
   images: any;
-  // goToSlide() {
-  //   this.slides.slideTo(1, 5000);
-  // }
+
+  
+  now = new Date().getTime();
   constructor(
     public navCtrl: NavController,
     public http: Http
@@ -24,24 +24,43 @@ export class HomePage {
     .subscribe(data=> {
       this.fetchedImages = data
       this.images = this.fetchedImages;
+    });
     }
-  );
-  }
+  
 
   refresh = setInterval(()=> {
     this.http.get('http://solosnet.com/signage/test.cfm')
     .map(res=> res.json())
     .subscribe(data=> {
-      this.fetchedImages = data
-      console.log('IMAGES', this.fetchedImages.length);
-      this.images = this.fetchedImages;
+      this.images = data;
+      // this.images.forEach(element => {
+      //   console.log(this.images.indexOf(element));
+      //   this.goToNext(element.index, element.DATETIME);
+      // });
+      // console.log('Objects', this.images);
+      // var nextTime = this.images[1].DATETIME;
+      // var eta_ms = nextTime - Date.now();
+      // setTimeout(()=>{
+      //   this.goToSlide(1)
+      // }, eta_ms);
+      // console.log('ETA', eta_ms);
     });
-  }, 5000);
+  }, 10000);
 
-  
+  isTime: boolean = false;
+  if ()
+  goToNext(index, time) {
+    var eta_ms = time - Date.now();
+    setTimeout(()=> {
+      this.slides.slideNext();
+      console.log('Fired!');
+    }, eta_ms);
+  }
+
+
 
   download() {
-    var fileTransfer = new Transfer();
+    // var fileTransfer = new Transfer();
   }
 
 }
