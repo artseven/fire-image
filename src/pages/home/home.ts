327cd72f3284efd384e91baa1ad0bbbd76076e26
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-13 17:28:57 -0500

    updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,32 +23,38 @@ export class HomePage {
       this.fetchedImages = data
       this.images = this.fetchedImages;
     });
-    }
-  
+  }
 
-  refresh = setInterval(()=> {
-    this.http.get('http://solosnet.com/signage/test.cfm')
-    .map(res=> res.json())
-    .subscribe(data=> {
-      this.images = data;
-      if (this.images[1].DATETIME - Date.now() > 0 ) {
-          this.goToNext(1, this.images[1].DATETIME);
-      }
-    });
-  }, 10000);
 
   goToNext(index, time) {
-    var eta_ms = time - Date.now();
+    var multiplied = time*1000;
+    var eta_ms = multiplied - Date.now();
+    console.log('DATE.NOW', new Date().getTime());
+    console.log('TIME', multiplied);
+    console.log(eta_ms);
     setTimeout(()=> {
       this.slides.slideNext();
       console.log('Fired!');
     }, eta_ms);
   }
 
+  refresh = setInterval(()=> {
+    this.http.get('http://solosnet.com/signage/test.cfm')
+    .map(res=> res.json())
+    .subscribe(data=> {
+      this.images = data;
+      this.goToNext(1, this.images[1].DATETIME);
+      });
+  }, 10000);
+
+
+
   slideChanged() {
-    let currentIndex = this.slides.getActiveIndex();
-    console.log('Current index is ', currentIndex);
-    this.goToNext(currentIndex + 1, this.images(currentIndex+1).DATETIME);
+    // let currentIndex = this.slides.getActiveIndex();
+    // let nextIndex = currentIndex + 1;
+    // console.log('Current index is ', currentIndex);
+
+    // this.goToNext(nextIndex, this.images[nextIndex].DATETIME);
   }
 
   download() {
